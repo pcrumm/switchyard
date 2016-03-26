2905d6fec485f749238fdf5bda0441d976912d05
Phil Crumm <pkcrumm@gmail.com>
2016-03-25 18:10:22 -0700

    Add an additional test to test multiple routes pointing to the same endpoint.
    Further, add descriptions for the purposes of each test route.

--- tests/sample-application/route-aliases.js
+++ tests/sample-application/route-aliases.js
@@ -3,8 +3,9 @@ module.exports = {
      * We specify aliases in the format 'alias path':'map path'.
      * Currently, aliases apply to /all/ matching verbs for the path.
      */
-    '/alias-test': '/test',
-    '/alias-test/deep/path': '/test/testing',
-    '/testing/named/:id': '/test/named/:id',
-    '/broken-path-test': '/endpoint/is/fake'
+    '/alias-test': '/test', // To test basic aliasing
+    '/alias-2-test': '/test', // To test multiple aliasing
+    '/alias-test/deep/path': '/test/testing', // To verify path independence
+    '/testing/named/:id': '/test/named/:id', // To test named routes
+    '/broken-path-test': '/endpoint/is/fake' // To ensure error conditions are preserved
 };

20493ba1f63ff74f9addcf4ed991c8290c121574
Phil Crumm <pkcrumm@gmail.com>
2016-03-25 18:19:35 -0700

    Add additional test aliases to verify that '/' prefixing is not required.

--- tests/sample-application/route-aliases.js
+++ tests/sample-application/route-aliases.js
@@ -7,5 +7,8 @@ module.exports = {
     '/alias-2-test': '/test', // To test multiple aliasing
     '/alias-test/deep/path': '/test/testing', // To verify path independence
     '/testing/named/:id': '/test/named/:id', // To test named routes
-    '/broken-path-test': '/endpoint/is/fake' // To ensure error conditions are preserved
+    '/broken-path-test': '/endpoint/is/fake', // To ensure error conditions are preserved
+    'no-prefix-test': '/test/testing', // To verify that aliases are not required to have a prefix
+    '/true-no-prefix-test': 'test', // To verify that true routes are not required to have a prefix
+    'both-have-no-prefix': 'test/testing', // To verify that both routes are not required to have a prefix
 };

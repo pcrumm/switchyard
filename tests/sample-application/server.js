7250c0f7f1d5abb0fd9835cc46a03781b6924d6c
Phil Crumm <pkcrumm@gmail.com>
2016-03-25 18:38:45 -0700

    Include route aliases in our test conditions.

--- tests/sample-application/server.js
+++ tests/sample-application/server.js
@@ -1,6 +1,6 @@
 var app = require( 'express' )();
 var switchyard = require( './../../lib/switchyard.js' );
 
-switchyard( app, __dirname + '/controllers' );
+switchyard( app, __dirname + '/controllers', require( './route-aliases.js' ) );
 
 module.exports = app.listen( 40595, function() { } );
